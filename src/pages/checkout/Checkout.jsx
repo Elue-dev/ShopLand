08c88b9dec4490f2aa25b26860b544f134512c8e
Wisdom Elue <mac@Wisdoms-MacBook-Pro.local>
2022-07-13 02:12:46 +0100

    changes

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,3 +1,101 @@
+// import React, { useState, useEffect } from "react";
+// import { loadStripe } from "@stripe/stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   CALCULATE_SUBTOTAL,
+//   CALCULATE_TOTAL_QUANTITY,
+//   selectCartItems,
+//   selectCartTotalAmounts,
+// } from "../../redux/slice/cartSlice";
+// import { selectEmail } from "../../redux/slice/authSlice";
+// import {
+//   selectBillingAddress,
+//   selectShippingAddress,
+// } from "../../redux/slice/checkoutSlice";
+// import { toast } from "react-toastify";
+// import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
+
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+
+// const Checkout = () => {
+//   const [message, setMessage] = useState("Initializing checkout...");
+//   const [clientSecret, setClientSecret] = useState("");
+
+//   const cartItems = useSelector(selectCartItems);
+//   const totalAmount = useSelector(selectCartTotalAmounts);
+//   const customerEmail = useSelector(selectEmail);
+
+//   const shippingAddress = useSelector(selectShippingAddress);
+//   const billingAddress = useSelector(selectBillingAddress);
+
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(CALCULATE_SUBTOTAL());
+//     dispatch(CALCULATE_TOTAL_QUANTITY());
+//   }, [dispatch, cartItems]);
+
+//   const description = `ShopLand  payment: email: ${customerEmail}, Amount: ${totalAmount}`;
+
+//   useEffect(() => {
+//     // Create PaymentIntent as soon as the page loads
+//     fetch("https://localhost:4242/create-payment-intent", {
+//       mode: 'no cors',
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         items: cartItems,
+//         userEmail: customerEmail,
+//         shipping: shippingAddress,
+//         billing: billingAddress,
+//         description,
+//       }),
+//     })
+//       .then((res) => {
+//         if (res.ok) {
+//           return res.json();
+//         }
+//         return res.json().then((json) => Promise.reject(json));
+//       })
+//       .then((data) => {
+//         setClientSecret(data.clientSecret);
+//       })
+//       .catch((error) => {
+//         setMessage("Failed to initialize checkout. Please try again.");
+//         toast.error(
+//           "Something went wrong, this could be due to network issues",
+//           {
+//             autoClose: 5000,
+//             pauseOnFocusLoss: false,
+//           }
+//         );
+//       });
+//   }, [billingAddress, shippingAddress, customerEmail, cartItems, description]);
+
+//   const appearance = {
+//     theme: "stripe",
+//   };
+//   const options = {
+//     clientSecret,
+//     appearance,
+//   };
+
+//   return (
+//     <>
+//       <section>
+//         <div className="container">{!clientSecret && <h3>{message}</h3>}</div>
+//       </section>
+//       {clientSecret && (
+//         <Elements options={options} stripe={stripePromise}>
+//           <CheckoutForm />
+//         </Elements>
+//       )}
+//     </>
+//   );
+// };
+
+// export default Checkout;
+
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -16,7 +114,7 @@ import {
 import { toast } from "react-toastify";
 import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK );
 
 const Checkout = () => {
   const [message, setMessage] = useState("Initializing checkout...");
@@ -35,12 +133,12 @@ const Checkout = () => {
     dispatch(CALCULATE_TOTAL_QUANTITY());
   }, [dispatch, cartItems]);
 
-  const description = `ShopLand  payment: email: ${customerEmail}, Amount: ${totalAmount}`;
+  const description = `eShop payment: email: ${customerEmail}, Amount: ${totalAmount}`;
 
   useEffect(() => {
+    // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
-    // "https://localhost:4242/create-payment-intent"
-    fetch("https://shoplandcomm.herokuapp.com/create-payment-intent", {
+    fetch("http://shoplandcomm.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -61,16 +159,10 @@ const Checkout = () => {
         setClientSecret(data.clientSecret);
       })
       .catch((error) => {
-        setMessage("Failed to initialize checkout. Please try again.");
-        toast.error(
-          "Something went wrong, this could be due to network issues",
-          {
-            autoClose: 5000,
-            pauseOnFocusLoss: false,
-          }
-        );
+        setMessage("Failed to initialize checkout");
+        toast.error("Something went wrong!!!");
       });
-  }, [billingAddress, shippingAddress, customerEmail, cartItems, description]);
+  }, []);
 
   const appearance = {
     theme: "stripe",
