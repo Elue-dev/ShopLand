06ac7a22754dec60b070c358bad5cea8db783e8d
Wisdom <eluewisdom@gmail.com>
2022-08-05 20:08:36 +0100

    added offline status functionality

--- src/App.js
+++ src/App.js
@@ -29,132 +29,142 @@ import { useSelector } from "react-redux";
 import { selectSuccessURL } from "./redux/slice/orderSlice";
 import RedirectSuccess from "./components/RedirectSuccess";
 import ChatBot from "./pages/home/ChatBot";
-import styles from './pages/home/home.module.scss'
+import styles from "./pages/home/home.module.scss";
 import { SiGooglemessages } from "react-icons/si";
+import useNetworkStatus from "./hooks/useNetworkStatus";
+import OfflinePage from "./components/offlinePage/OfflinePage";
 
 function App() {
   const successUrl = useSelector(selectSuccessURL);
   const [show, setShow] = useState(false);
 
+  const status = useNetworkStatus();
+
   const handleBotVisibility = () => {
     setShow(!show);
   };
 
-
   return (
     <>
-      <BrowserRouter>
-        <ScrollToTop />
-        <ToastContainer
-          toastStyle={{
-            backgroundColor: "#c07d53",
-            color: "#fff",
-            fontSize: "1.6rem",
-          }}
-        />
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/reset" element={<Reset />} />
-          <Route
-            path="/delete-account"
-            element={
-              <ProtectedRoute>
-                <DeleteAccount />
-              </ProtectedRoute>
-            }
+      {!status ? (
+        <OfflinePage />
+      ) : (
+        <BrowserRouter>
+          <ScrollToTop />
+          <ToastContainer
+            toastStyle={{
+              backgroundColor: "#c07d53",
+              color: "#fff",
+              fontSize: "1.6rem",
+            }}
           />
-          <Route
-            path="/admin/*"
-            element={
-              <AdminOnlyRoute>
-                <Admin />
-              </AdminOnlyRoute>
-            }
-          />
-          <Route path="/product-details/:id" element={<ProductDetails />} />
+          <Header />
 
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/saved-products" element={<Saved />} />
-          <Route
-            path="/notifications"
-            element={
-              <ProtectedRoute>
-                <Notifications />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/checkout-details"
-            element={
-              <ProtectedRoute>
-                <CheckoutDetails />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/checkout"
-            element={
-              <ProtectedRoute>
-                <Checkout />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/checkout-success"
-            element={
-              successUrl.includes("checkout") ? (
-                <CheckoutSuccess />
-              ) : (
-                <RedirectSuccess />
-              )
-            }
-          />
-          <Route
-            path="/order-history"
-            element={
-              <ProtectedRoute>
-                <OrderHistory />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/order-details/:id"
-            element={
-              <ProtectedRoute>
-                <OrderDetails />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/review-product/:id"
-            element={
-              <ProtectedRoute>
-                <ReviewProducts />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/reset" element={<Reset />} />
+            <Route
+              path="/delete-account"
+              element={
+                <ProtectedRoute>
+                  <DeleteAccount />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/admin/*"
+              element={
+                <AdminOnlyRoute>
+                  <Admin />
+                </AdminOnlyRoute>
+              }
+            />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
 
-        <div className={styles.home}>
-        <div
-          className={show ? `${styles.bot} ${styles.show}` : `${styles.bot}`}
-        >
-          <ChatBot />
-        </div>
-        <div className={styles.button}>
-          <button onClick={handleBotVisibility}>
-            <SiGooglemessages className={styles["bot-icon"]} />
-          </button>
-        </div>
-        </div>
-       
-        <Footer />
-      </BrowserRouter>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/saved-products" element={<Saved />} />
+            <Route
+              path="/notifications"
+              element={
+                <ProtectedRoute>
+                  <Notifications />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/checkout-details"
+              element={
+                <ProtectedRoute>
+                  <CheckoutDetails />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/checkout"
+              element={
+                <ProtectedRoute>
+                  <Checkout />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/checkout-success"
+              element={
+                successUrl.includes("checkout") ? (
+                  <CheckoutSuccess />
+                ) : (
+                  <RedirectSuccess />
+                )
+              }
+            />
+            <Route
+              path="/order-history"
+              element={
+                <ProtectedRoute>
+                  <OrderHistory />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/order-details/:id"
+              element={
+                <ProtectedRoute>
+                  <OrderDetails />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/review-product/:id"
+              element={
+                <ProtectedRoute>
+                  <ReviewProducts />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+
+          <div className={styles.home}>
+            <div
+              className={
+                show ? `${styles.bot} ${styles.show}` : `${styles.bot}`
+              }
+            >
+              <ChatBot />
+            </div>
+            <div className={styles.button}>
+              <button onClick={handleBotVisibility}>
+                <SiGooglemessages className={styles["bot-icon"]} />
+              </button>
+            </div>
+          </div>
+
+          <Footer />
+        </BrowserRouter>
+      )}
     </>
   );
 }
